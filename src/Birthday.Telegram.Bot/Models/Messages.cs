dbe90f63d491915f03353595b848e0f672ecff2a
GNicoDev <nickholasdvp@gmail.com>
2022-02-03 22:44:51 +0300

    Доработки сообщений

--- src/Birthday.Telegram.Bot/Models/Messages.cs
+++ src/Birthday.Telegram.Bot/Models/Messages.cs
@@ -46,6 +46,36 @@ public static class Messages
 
 Примечание\. Я не читаю вашу переписку из чатов\, мне это совершенно не интересно\. Для того чтобы начать общение со мной в чате для этого есть специальные команды\.";
 
-    public static string MessageForGetBirthday(string userName, string fromChatName) => @$"Привет *{ToMarkdownV2(userName)}*\.
+    /// <summary>
+    /// Приветственное сообщение когда бот хочет получить дату рождения пользователя
+    /// </summary>
+    /// <param name="userName">Имя пользователя</param>
+    /// <param name="fromChatName">Откуда пришел пользователь</param>
+    /// <returns>Сообщение</returns>
+    public static string MessageForGetBirthdayDate(string userName, string fromChatName) => @$"Привет *{ToMarkdownV2(userName)}*\.
 Чтобы пользователи чата *{ToMarkdownV2(fromChatName)}* смогли поздравлять тебя мне нужно шепнуть дату твоего дня рождения\.";
+
+    /// <summary>
+    /// Приветственное сообщение от бота, когда он входит в главный чат
+    /// </summary>
+    /// <param name="botName">Название бота</param>
+    /// <param name="mainChatId">Идентификатор главного чата</param>
+    /// <returns>Приветственное сообщение</returns>
+    public static string HelloMessageFromBotToMainChat(string botName, string mainChatId) => @$"Привет всем\! 
+Я Ваш помощник для отслеживания дней рождений\.
+Чтобы начать вам помогать мне нужна некая информация
+Для этого\, пожалуйста\, перейдите по [этой](https://t.me/{botName}?start={mainChatId}) ссылочке и ответьте на парочку вопросов
+
+Я не читаю ваши переписки, мне это совершенно не нужно\.";
+
+    /// <summary>
+    /// Приветственное сообщение от бота, когда новый пользователь входит в чат
+    /// </summary>
+    /// <param name="newUserName">Имя нового пользователя чата</param>
+    /// <param name="botName">Название бота</param>
+    /// <param name="mainChatId">Идентификатор главного чата</param>
+    /// <returns>Приветственное сообщение</returns>
+    public static string HelloMessageForNewChatUser(string newUserName, string botName, string mainChatId) => @$"Привет *{newUserName}*\! 
+Твои друзья попросили меня следить за тем, у кого когда день рождение\.
+Чтобы я смог оповещать и о твоем дне рождения перейди\, пожалуйста\, по [этой](https://t.me/{botName}?start={mainChatId}) ссылочке и ответь на парочку вопросов";
 }
--- src/Birthday.Telegram.Bot/Services/BotChatMemberProcessor.cs
+++ src/Birthday.Telegram.Bot/Services/BotChatMemberProcessor.cs
@@ -1,3 +1,4 @@
+using Birthday.Telegram.Bot.Models;
 using Birthday.Telegram.Bot.Services.Abstractions;
 using Telegram.Bot;
 using Telegram.Bot.Types;
@@ -23,47 +24,96 @@ public class BotChatMemberProcessor : IBotChatMemberProcessor
     /// <inheritdoc cref="ProcessAsync"/>
     public async Task ProcessAsync(ChatMemberUpdated action, CancellationToken cancellationToken)
     {
-        var processStatus = action.NewChatMember.Status;
-
-        var processUpdate = processStatus switch 
-        {
-            ChatMemberStatus.Member => AddBotToChatAction(action.Chat, cancellationToken),
-            ChatMemberStatus.Left => RemoveBotFromChat(action.Chat, cancellationToken)
-        };
-
-        await processUpdate;
-    }
-
-    private async Task AddBotToChatAction(Chat chatInfo, CancellationToken cancellationToken)
-    {
         // Спросить текущую инфу о боте
         var botInfo = await _telegramBotClient.GetMeAsync(cancellationToken);
+        var processStatus = action.NewChatMember.Status;
+        if (botInfo.Id.Equals(action.NewChatMember.User.Id))
+        {
+            var processUpdate = processStatus switch
+            {
+                ChatMemberStatus.Member => AddBotToChatAction(chatInfo: action.Chat, 
+                    botInfo: botInfo, 
+                    cancellationToken: cancellationToken),
+                ChatMemberStatus.Left => RemoveBotFromChatAction(chatInfo: action.Chat, 
+                    cancellationToken: cancellationToken)
+            };
+            await processUpdate;
+        }
+        else
+        {
+            var processUpdate = processStatus switch
+            {
+                ChatMemberStatus.Member => AddNewMemberChatAction(chatInfo: action.Chat, 
+                    botInfo: botInfo,
+                    newUser: action.NewChatMember.User, 
+                    cancellationToken: cancellationToken),
+                ChatMemberStatus.Left => RemoveMemberFromChatAction(chatInfo: action.Chat, 
+                    cancellationToken: cancellationToken)
+            };
+            await processUpdate;
+        }
+    }
+
+    private async Task AddBotToChatAction(Chat chatInfo, User botInfo, CancellationToken cancellationToken)
+    {
         // Отправить приветственное сообщение
-        await SendHelloMessageToChat(chatInfo, botInfo, cancellationToken);
+        await SendHelloMessageFromBotToChat(chatInfo, botInfo, cancellationToken);
         // Записать в БД данные о чате
         // Связать данные о чате с пользователями
     }
 
-    private Task RemoveBotFromChat(Chat chatInfo, CancellationToken cancellationToken)
+    /// <summary>
+    /// Действия при удалении бота из чата
+    /// </summary>
+    /// <param name="chatInfo">Информация о чате из которого удалили бота</param>
+    /// <param name="cancellationToken">Токен отмены операции</param>
+    /// <returns>Задача</returns>
+    private Task RemoveBotFromChatAction(Chat chatInfo, CancellationToken cancellationToken)
     {
-        // При удалении 
+        // При удалении нужно вычистить все из бд
 
         return Task.CompletedTask;
     }
 
-    private async Task SendHelloMessageToChat(Chat chatInfo, User botInfo, CancellationToken cancellationToken)
+    /// <summary>
+    /// Действие при добавлении нового пользователя в чат
+    /// </summary>
+    /// <param name="chatInfo">Информация о чате в который добавлен пользователь</param>
+    /// <param name="newUser">Информация о новом пользователе</param>
+    /// <param name="botInfo">Информация о новом боте</param>
+    /// <param name="cancellationToken">Токен отмены операции</param>
+    /// <returns>Задача</returns>
+    private async Task AddNewMemberChatAction(Chat chatInfo, User botInfo, User newUser, CancellationToken cancellationToken)
     {
-        var helloMessageString = @$"Привет всем\! 
-Я Ваш помощник для отслеживания дней рождений\.
-Чтобы начать вам помогать мне нужна некая информация
-Для этого\, пожалуйста\, перейдите по [этой](https://t.me/{botInfo.Username}?start={chatInfo.Id}) ссылочке и ответьте на парочку вопросов
-
-Я не читаю ваши переписки, мне это совершенно не нужно\.";
-
-
-        await _telegramBotClient.SendTextMessageAsync(chatInfo.Id, 
-                        helloMessageString, 
-                        ParseMode.MarkdownV2,
-                        cancellationToken: cancellationToken);
+        // Приветствуем нового пользователя чата
+        await SendHelloMessageToNewChatMember(chatInfo: chatInfo, 
+            newUser: newUser, 
+            botInfo: botInfo, 
+            cancellationToken: cancellationToken);
+        // Записываем данные о пользователе в БД и связываем его с чатом
     }
+
+    /// <summary>
+    /// Действие при удалении пользователя из чата
+    /// </summary>
+    /// <param name="chatInfo">Информация о чате из которого удален пользователь</param>
+    /// <param name="cancellationToken">Токен отмены операции</param>
+    /// <returns>Задача</returns>
+    private Task RemoveMemberFromChatAction(Chat chatInfo, CancellationToken cancellationToken)
+    {
+        // Удаляем пользователя из БД и удаляем связку с ним
+        return Task.CompletedTask;
+    }
+
+    private Task SendHelloMessageFromBotToChat(Chat chatInfo, User botInfo, CancellationToken cancellationToken) =>
+        _telegramBotClient.SendTextMessageAsync(chatInfo.Id,
+                        Messages.HelloMessageFromBotToMainChat(botInfo.Username!, chatInfo.Id.ToString()),
+                        Messages.ParseMode,
+                        cancellationToken: cancellationToken);
+
+    private Task SendHelloMessageToNewChatMember(Chat chatInfo, User botInfo, User newUser, CancellationToken cancellationToken) =>
+        _telegramBotClient.SendTextMessageAsync(chatInfo.Id,
+                        Messages.HelloMessageForNewChatUser(newUser.Username!, botInfo.Username!, chatInfo.Id.ToString()),
+                        Messages.ParseMode,
+                        cancellationToken: cancellationToken);
 }
